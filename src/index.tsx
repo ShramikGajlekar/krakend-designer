4a64cf0b1bf6f048bd6f2442e0115e59f72d80ad
GaneshKathar <katharganesh27@gmail.com>
2021-06-20 18:11:27 +0530

    add initial setup with react and typescript| Ganesh

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <React.StrictMode>Hello, Start coding to see magic</React.StrictMode>,
+  document.getElementById("root")
+);
